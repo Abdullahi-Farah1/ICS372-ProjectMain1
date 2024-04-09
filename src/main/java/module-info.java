6a7f01eda005fcddc0b6796c5edc0b35a51f6635
Austin Lyksett <alyksett1029@gmail.com>
2024-04-08 23:52:57 -0500

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,10 @@ module aya.reviews.ics372projectmain1 {
 
     opens aya.reviews.ics372projectmain1 to javafx.fxml;
     exports aya.reviews.ics372projectmain1;
+    exports aya.reviews.ics372projectmain1.database;
+    opens aya.reviews.ics372projectmain1.database to javafx.fxml;
+    exports aya.reviews.ics372projectmain1.datamodels;
+    opens aya.reviews.ics372projectmain1.datamodels to javafx.fxml;
+    exports aya.reviews.ics372projectmain1.uimodels;
+    opens aya.reviews.ics372projectmain1.uimodels to javafx.fxml;
 }
