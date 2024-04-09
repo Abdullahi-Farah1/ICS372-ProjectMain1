2e636981005f7e165aaaff768b31616526adcd44
Austin Lyksett <alyksett1029@gmail.com>
2024-04-08 23:52:09 -0500

    create data models

---
+++ src/main/java/aya/reviews/ics372projectmain1/datamodels/Movie.java
@@ -0,0 +1,19 @@
+package aya.reviews.ics372projectmain1.datamodels;
+
+
+public class Movie extends Media{
+    private int runtime;
+
+    public Movie(String name, String description, int runtime){
+        super(name, description);
+        setRuntime(runtime);
+    }
+
+    public int getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(int runtime) {
+        this.runtime = runtime;
+    }
+}
