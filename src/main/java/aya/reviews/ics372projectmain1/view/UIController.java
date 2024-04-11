0d9d9dc39851448bef3729d71620a776708825d5
Abdullahi Farah <abdullahi.ali.farah4@gmail.com>
2024-04-10 23:09:31 -0500

    UIcontrol change

--- src/main/java/aya/reviews/ics372projectmain1/view/UIController.java
+++ src/main/java/aya/reviews/ics372projectmain1/view/UIController.java
@@ -6,16 +6,17 @@ import aya.reviews.ics372projectmain1.MainApp;
 import aya.reviews.ics372projectmain1.control.UserController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class UIController {
+    @FXML
+    private TextArea reviewInput;
+    @FXML
+    private TextField ratingInput;
     @FXML
     private Button button;
     @FXML
@@ -77,4 +78,23 @@ public class UIController {
             }
         }
     }
+    public void reviewAndRate() {
+        // Retrieve the user input from the text fields
+        String review = reviewInput.getText();
+        int rating = Integer.parseInt(ratingInput.getText());
+
+        // Do something with the user input (e.g., store it in variables, process it, etc.)
+        System.out.println("Review: " + review);
+        System.out.println("Rating: " + rating);
+
+
+    }
+
+    public void cancelButton(ActionEvent event) throws IOException {
+        MainApp s = new MainApp();
+        s.changeScene("afterLogin.fxml");
+
+
+    }
+
 }
